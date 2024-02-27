2615ecaa2dfb214f0b2382d1d051eb8511d76095
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-02-26 22:41:22 -0300

    Implementando funcionalidade de pagamentos

---
+++ DevFreela.Payments.API/Models/PaymentInfoInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DevFreela.Payments.API.Models
+{
+    public class PaymentInfoInputModel
+    {
+        public int IdProject { get; set; }
+        public string CreditCardNumber { get; set; } = string.Empty;
+        public string Cvv { get; set; } = string.Empty;
+        public string ExpiresAt { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+    }
+}
--- DevFreela.Payments.API/Program.cs
+++ DevFreela.Payments.API/Program.cs
@@ -1,11 +1,15 @@
+using DevFreela.Payments.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IPaymentService, PaymentService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
